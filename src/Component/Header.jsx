717ae4ea136da0752a12108d26c4c1a0e1bfb114
Mohammad Dinar <web.dev.dinar@gmail.com>
2023-12-15 16:57:50 +0600

    Responsive toggoling feature added for mobile device

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -4,10 +4,14 @@ import {
   HiPlayCircle,
   HiPlus,
   HiTv,
+  
 } from "react-icons/hi2";
 import HeaderItem from "./HeaderItem";
+import { HiOutlineDotsVertical } from "react-icons/hi"
+import { useState } from "react";
 
 const Header = () => {
+    const [toggle , setToggle] = useState(false)
   const menu = [
     {
       name: "HOME",
@@ -29,16 +33,20 @@ const Header = () => {
       name: "SERIES",
       icon: HiTv,
     },
+    {
+      name: "ORIGNALS",
+      icon: HiPlus,
+    },
   ];
   return (
     <div className="flex justify-between items-center container mx-auto">
-      <div className="flex items-center gap-7">
+      <div className="flex items-center md:gap-7">
         <img
           src="https://i.postimg.cc/PJWWV5tZ/images-removebg-preview.png"
           alt="logo"
           className="w-[80px] md:w-[120px] object-cover"
         />
-        <div className="flex mt-3">
+        <div className="hidden md:flex mt-3 gap-8">
           {menu.map((item) => (
             <HeaderItem
               key={item.name}
@@ -47,11 +55,38 @@ const Header = () => {
             ></HeaderItem>
           ))}
         </div>
+        <div className=" flex md:hidden mt-3 gap-3">
+          {menu.map(
+            (item, index) =>
+              index < 3 && (
+                <HeaderItem
+                  key={item.name}
+                  name={''}
+                  Icon={item.icon}
+                ></HeaderItem>
+              )
+          )}
+          <div className="md:hidden" onClick={()=>setToggle(!toggle)}>
+            <HeaderItem Icon={HiOutlineDotsVertical}></HeaderItem>
+          </div>
+          {toggle ?<div className="mt-6 mb-2 absolute ml-20 border my-2 bg-[#121212]  p-4 ">
+          {menu.map(
+            (item, index) =>
+              index > 2 && (
+                <HeaderItem
+                  key={item.name}
+                  name={item.name}
+                  Icon={item.icon}
+                ></HeaderItem>
+              )
+          )}
+          </div>: null}
+        </div>
       </div>
       <img
         src="https://i.postimg.cc/Hsk4KD86/imgbin-computer-icons-portable-network-graphics-avatar-icon-design-avatar-Ds-Z54-Du30h-Tr-Kfx-BG5-Pbwvzg-E.png"
         alt=""
-        className="rounded-full w-[60px] h-[40px] mt-3"
+        className="rounded-full w-[40px] md:w-[60px] h-[30px] md:h-[40px] mt-3"
       />
     </div>
   );
--- src/Component/HeaderItem.jsx
+++ src/Component/HeaderItem.jsx
@@ -1,11 +1,12 @@
+/* eslint-disable react/prop-types */
 
 
 const HeaderItem = ({name, Icon}) => {
     return (
-        <div className="flex text-white text-[15px] font-semibold gap-3 items-center">
-            <div className="flex items-center ml-4 gap-3 hover:underline underline-offset-8">
+        <div className="flex  text-white text-[15px] mb-2 font-semibold gap-3 items-center">
+            <div className="flex items-center md:ml-4 gap-3 hover:underline underline-offset-8">
             <Icon></Icon>
-            <h2>{name}</h2>
+            <h2 className="">{name}</h2>
             </div>
         </div>
     );
